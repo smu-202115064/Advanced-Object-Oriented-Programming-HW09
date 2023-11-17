495513ec2f09b4771d606ebb26db4b960e6db17d
Hepheir <hepheir@gmail.com>
2023-11-17 16:53:29 +0900

    feat(Operator): processDigit() 메소드 구현 (Operand2 상태로 전이)

--- src/Operator.java
+++ src/Operator.java
@@ -4,4 +4,10 @@ public class Operator implements State {
     public Operator(Calculator calculator) {
         this.calculator = calculator;
     }
+
+    @Override
+    public void processDigit(int digit) {
+        calculator.setOperand2(digit);
+        calculator.setState(new Operand2(calculator));
+    }
 }
