4d0d5e9b9071a896e1d3b4f08b70fdb832881b85
Hepheir <hepheir@gmail.com>
2023-11-17 16:52:09 +0900

    feat(Start): processDigit() 메소드 구현 (Operand1 상태로 전이)

--- src/Start.java
+++ src/Start.java
@@ -4,4 +4,10 @@ public class Start implements State {
     public Start(Calculator calculator) {
         this.calculator = calculator;
     }
+
+    @Override
+    public void processDigit(int digit) {
+        calculator.setOperand1(digit);
+        calculator.setState(new Operand1(calculator));
+    }
 }
