61284db5ac32c19f4299db9020800690945c182f
Hepheir <hepheir@gmail.com>
2023-11-17 16:43:42 +0900

    feat(Main): 과제 요구사항에 맞도록 main() 메소드 구현

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+
         char ch = sc.next().charAt(0);      // 키보드에서 한 글자 입력 받기
         while (ch != 'q' && ch != 'Q') {    // 종료 문자가 아니면 반복
+            if (ch >= '0' && ch <= '9') {
+                calculator.processDigit(ch - '0');
+            }
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+                calculator.processArithmeticOperator(ch);
+            }
+            else if (ch == '=') {
+                calculator.processEqualOperator();
+                System.out.printf("%d %c %d = %d\n", calculator.getOperand1(), calculator.getOperator(), calculator.getOperand2(), calculator.getResult());
+                calculator.setOperand1(0);
+                calculator.setOperand2(0);
+            }
+            else {
+                System.out.println("invalid operation");
+            }
+            ch = sc.next().charAt(0);
         }
     }
 }
